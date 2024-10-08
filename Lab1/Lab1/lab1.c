344581be27180a6c47387c557c9793b27aa3c787
juremartinic02 <jmarti02@fesb.hr>
2024-10-08 20:27:42 +0200

    Lab 1 added count_rows function

--- Lab1/Lab1/lab1.c
+++ Lab1/Lab1/lab1.c
@@ -1,8 +1,47 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
+#include<stdlib.h>
+
+#define ERROR_OPPENING_FILE -1
+
+//Stavimo 1024 jer je to maksimalni broj znakova u redu
+#define BUFFER_SIZE 1024
+
+struct _student;
+typedef struct _student {
+	char ime[100];
+} Student;
+
+int count_rows(char* filename);
 
 int main()
 {
 	printf("Hello world!\n");
 
-	return 0;
+	return EXIT_SUCCESS;
+}
+
+int count_rows(char* filename)
+{
+	FILE* file = NULL;
+
+	char buffer[BUFFER_SIZE];
+	int count = 0;
+
+	file = fopen(filename, "r");
+	if (!file)
+	{
+		printf("Error opening file!\n");
+		return ERROR_OPPENING_FILE;
+	}
+
+	while (!feof(file))
+	{
+		fgets(buffer, BUFFER_SIZE, file);
+		++count;
+	}
+
+
+	fclose(file);
+	return EXIT_SUCCESS;
 }
