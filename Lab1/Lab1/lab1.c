e0804b2611903e57730551bfae620167cecde5f8
juremartinic02 <jmarti02@fesb.hr>
2024-10-15 17:43:08 +0200

    Lab1 finalna verzija - dodana allocate_student funkcija

--- Lab1/Lab1/lab1.c
+++ Lab1/Lab1/lab1.c
@@ -1,149 +1,54 @@
-//#define _CRT_SECURE_NO_WARNINGS
-//#include<stdio.h>
-//#include<stdlib.h>
-//
-//#define ERROR_OPENING_FILE -1
-//
-//#define MAX_NAME_LENGTH 100
-//
-////Stavimo 1024 jer je to maksimalni broj znakova u redu
-//#define BUFFER_SIZE 1024
-//
-////Deklaracija studenta kao strukture
-//struct _student;
-//typedef struct Student* StudentPos;
-//typedef struct _student {
-//	char ime[MAX_NAME_LENGTH];
-//	char prezime[MAX_NAME_LENGTH];
-//	double brojBodova;
-//} Student;
-//
-////Definiramo StudentPos kao pokazivac na strukturu zbog ponavljanja kroz kod
-//
-//
-////Funkcija koja broji broj redaka u file-u
-//int count_rows(char* filename);
-//
-//StudentPos read_students(char* filename, int numStudents, double* maxBodovi);
-//void print_students(StudentPos students, int numStudents, double maxBodovi);
-//
-//int main()
-//{
-//    char filename[MAX_NAME_LENGTH] = "studenti.txt"; // Promijenite ime datoteke po potrebi
-//    int numStudents = 0;
-//    double maxBodovi = 0.0;
-//
-//    //numStudents = count_rows(filename);
-//    if (numStudents <= 0)
-//    {
-//        printf("Error reading the file or no students in the file.\n");
-//        return ERROR_OPENING_FILE;
-//    }
-//
-//    StudentPos students = read_students(filename, numStudents, &maxBodovi);
-//    if (!students)
-//    {
-//        printf("Error reading students.\n");
-//        return ERROR_OPENING_FILE;
-//    }
-//
-//    print_students(students, numStudents, maxBodovi);
-//
-//    free(students); // Oslobadanje dinamicki alocirane memorije
-//    return EXIT_SUCCESS;
-//}
-//
-//int count_rows(char* filename)
-//{
-//	FILE* file = NULL;
-//
-//	char buffer[BUFFER_SIZE];
-//	int count = 0;
-//
-//	file = fopen(filename, "r");
-//	if (!file)
-//	{
-//		printf("Error opening file!\n");
-//		return ERROR_OPENING_FILE;
-//	}
-//
-//	while (!feof(file))
-//	{
-//		fgets(buffer, BUFFER_SIZE, file);
-//		++count;
-//	}
-//
-//
-//	fclose(file);
-//	return EXIT_SUCCESS;
-//}
-//
-//StudentPos read_students(char* filename, int numStudents, double* maxBodovi)
-//{
-//    FILE* file = NULL;
-//    StudentPos students = NULL;
-//    int i = 0;
-//
-//    students = (StudentPos)malloc(numStudents * sizeof(Student));
-//    if (!students)
-//    {
-//        printf("Memory allocation failed!\n");
-//        return NULL;
-//    }
-//
-//    file = fopen(filename, "r");
-//    if (!file)
-//    {
-//        printf("Error opening file!\n");
-//        free(students);
-//        return NULL;
-//    }
-//
-//    for (i = 0; i < numStudents; ++i)
-//    {
-//        fscanf(file, "%s %s %lf", students[i].ime, students[i].prezime, &students[i].brojBodova);
-//        if (students[i].brojBodova > *maxBodovi)
-//        {
-//            *maxBodovi = students[i].brojBodova; // Pracenje maksimalnog broja bodova
-//        }
-//    }
-//
-//    fclose(file);
-//    return students;
-//}
-//
-//void print_students(StudentPos students, int numStudents, double maxBodovi)
-//{
-//    int i = 0;
-//    printf("Ime\tPrezime\t\tApsolutni Bodovi\tRelativni Bodovi\n");
-//    for (i = 0; i < numStudents; ++i)
-//    {
-//        double relativniBodovi = (students[i].brojBodova / maxBodovi) * 100;
-//        printf("%s\t%s\t\t%.2lf\t\t\t%.2lf%%\n", students[i].ime, students[i].prezime, students[i].brojBodova, relativniBodovi);
-//    }
-//}
-
+// Direktiva kojom iskljucujemo upozorenje za koristenje funckija za manipulaciju datoteka i stringova
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<stdlib.h>
 
 #define ERROR_OPENING_FILE -1
+#define ERROR_ALLOCATING_MEMORY -2
 #define MAX_NAME_LENGTH 100
 
-//Stavimo 1024 jer je to maksimalni broj znakova u redu
+// Stavimo 1024 jer je to maksimalni broj znakova u redu
 #define BUFFER_SIZE 1024
 
+// Definiranje strukture student
 typedef struct _student {
     char ime[MAX_NAME_LENGTH];
     char prezime[MAX_NAME_LENGTH];
     double brojBodova;
 } Student;
 
+// Definiramo StudentPos kao pokazivac na strukturu zbog citljivosti koda
 typedef Student* StudentPos;
+
+/*
+    Funckija: count_rows
+    --------------------------
+        Vraca broj redaka u datoteci.
+        (u ovom slucaju formatirana na nacin da 1 redak = 1 student sto kasnije koristimo da bi izracunali broj studenata)
+*/
 
 int count_rows(char* filename);
 
+/*
+    Funckija: allocate_students
+    ------------------------------------
+    Vraca pokazivac na alociranu memoriju.
+*/
+
+StudentPos allocate_students(int numStudents);
+
+/*
+    Funkcija: read_students
+    --------------------------------
+    Vraca pokazivac na niz struktura Student (u slucaju uspjesne alokacije memorije)
+*/
 StudentPos read_students(char* filename, int numStudents, double* maxBodovi);
+
+/*
+    Funkcija: print_students
+    ---------------------------------
+    Ispisuje korisniku na ekran procitane i obradjene podatke.
+*/
 void print_students(StudentPos students, int numStudents, double maxBodovi);
 
 int main()
@@ -155,7 +60,7 @@ int main()
     numStudents = count_rows(filename);
     if (numStudents <= 0)
     {
-        printf("Error reading the file or no students in the file.\n");
+        printf("Neuspjesno citanje datoteke ili nema studenata u datoteci za procitati.\n");
         return ERROR_OPENING_FILE;
     }
 
@@ -174,7 +79,10 @@ int main()
 
 int count_rows(char* filename)
 {
+    // Osigurava cinjenicu da datoteka postoji / se otvorila, u suprotnom pointer skuplja trash vrijednost
     FILE* file = NULL;
+
+    // "Cuva" redak teksta prije nego sto program procita drugi. Bez ovoga fgets nema gdje smjestiti prvi procitani redak
     char buffer[BUFFER_SIZE];
     int count = 0;
 
@@ -187,6 +95,7 @@ int count_rows(char* filename)
 
     while (fgets(buffer, BUFFER_SIZE, file))
     {
+        // Pitanje: da li bi mi ovdje mogli staviti count++?
         ++count;
     }
 
@@ -194,34 +103,63 @@ int count_rows(char* filename)
     return count;
 }
 
+StudentPos allocate_students(int numStudents)
+{
+    StudentPos students = (StudentPos)malloc(numStudents * sizeof(Student));
+
+    if (!students)
+    {
+        printf("Nesupjela alokacija memorije!");
+        return ERROR_ALLOCATING_MEMORY;
+    }
+
+    return students;
+}
+
 StudentPos read_students(char* filename, int numStudents, double* maxBodovi)
 {
     FILE* file = NULL;
+
+    // Inicijaliziramo pokazivac na strukturu kako ne bi pokupio neku trash vrijednost prije alokacije memorije
     StudentPos students = NULL;
     int i = 0;
 
-    students = (StudentPos)malloc(numStudents * sizeof(Student));
+    students = allocate_students(numStudents);
     if (!students)
     {
-        printf("Memory allocation failed!\n");
-        return NULL;
+        return ERROR_ALLOCATING_MEMORY;
     }
 
     file = fopen(filename, "r");
     if (!file)
     {
         printf("Error opening file!\n");
+
+        // Dobra praksa je oslobadjati memoriju ako je neuspjelo otvaranje datoteke
         free(students);
         return NULL;
     }
 
-    for (i = 0; i < numStudents; ++i)
+    while (i < numStudents)
     {
-        fscanf(file, "%s %s %lf", students[i].ime, students[i].prezime, &students[i].brojBodova);
-        if (students[i].brojBodova > *maxBodovi)
+        // Pokazivac na trenutnog studenta kako mozemo koristit operator -> zbog preglednosti koda. U suprotnosti moramo pisati students[i].ime
+        StudentPos student = &students[i];
+
+        if (fscanf(file, "%s %s %lf", student->ime, student->prezime, &student->brojBodova) != 3)
         {
-            *maxBodovi = students[i].brojBodova;
+            printf("Neuspjelo citanje podataka iz datoteke!");
+            break;
         }
+
+        // Citanje stvarnog broja bodova sa memorije jer broj bodova pocinje od 0
+        // Ovo je nacin kako maksimalan broj bodova dobivamo tako da usporedjujemo broj bodova od studenata
+        // Mogli smo i izbjec ovo da smo define-ali proizvoljan maksimalni broj bodova na testu u main-u
+        if (student->brojBodova > *maxBodovi)
+        {
+            *maxBodovi = student->brojBodova;
+        }
+
+        i++;
     }
 
     fclose(file);
@@ -232,9 +170,10 @@ void print_students(StudentPos students, int numStudents, double maxBodovi)
 {
     int i = 0;
     printf("Ime\tPrezime\t\tApsolutni Bodovi\tRelativni Bodovi\n");
-    for (i = 0; i < numStudents; ++i)
+
+    for (i = 0; i < numStudents; i++)
     {
         double relativniBodovi = (students[i].brojBodova / maxBodovi) * 100;
-        printf("%s\t%s\t\t%.2lf\t\t\t%.2lf%%\n", students[i].ime, students[i].prezime, students[i].brojBodova, relativniBodovi);
+        printf("%s\t%s\t\t%.2f\t\t\t%.2f%%\n", students[i].ime, students[i].prezime, students[i].brojBodova, relativniBodovi);
     }
 }
